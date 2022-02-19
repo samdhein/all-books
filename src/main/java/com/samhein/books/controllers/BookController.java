e961d83acb0604f6e26c7548eb8028143a6333ea
samdhein <33975663+samdhein@users.noreply.github.com>
2022-02-18 21:14:03 -0800

    assignment complete

---
+++ src/main/java/com/samhein/books/controllers/BookController.java
@@ -0,0 +1,38 @@
+package com.samhein.books.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.samhein.books.models.Book;
+import com.samhein.books.services.BookService;
+
+@Controller
+public class BookController {
+
+	@Autowired
+	private BookService bookService;
+	
+	
+	@GetMapping("/books/{id}")
+	public String showBook(@PathVariable("id")Long id, Model model) {
+		Book book = bookService.findBook(id);
+		model.addAttribute("book", book);
+		return "show.jsp";
+	}
+	@GetMapping("/books")
+	public String allBooks(Model model) {
+		List<Book> books = bookService.allBooks();
+		model.addAttribute("books", books);
+		return "books.jsp";
+	}
+	
+	
+	
+	
+	
+}
